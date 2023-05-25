c9aa9e2fda6360244a446ba89fd2391d99642b29
Mudra Verma <md393822@dal.ca>
2023-05-25 06:04:04 -0300

    Source code for both containers running on localhost

--- A1/Container1/src/main/java/com/A1/Container1/Controller1.java
+++ A1/Container1/src/main/java/com/A1/Container1/Controller1.java
@@ -1,10 +1,97 @@
 package com.A1.Container1;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 public class Controller1 {
+
+    String responseReceived = null;
+
+    @PostMapping(path = "/calculate", consumes = "application/json")
+    public String receiveJson(@RequestBody String response){
+        try{
+            System.out.println("Received Json: " +response);
+            JSONObject jsonObject = new JSONObject(response.toString());
+            String fileName = jsonObject.getString("file").toString();
+            System.out.println("File: "+fileName);
+            String product = jsonObject.getString("product").toString();
+            System.out.println("Product: "+product);
+            responseReceived = generateResponse(fileName, response);
+        }
+        catch(Exception e) {
+            responseReceived = "Exception: " + e;
+            System.out.println("Exception: " + e);
+        }
+        return responseReceived;
+    }
+
+    public String generateResponse(String fileName, String response){
+        if(validFile(fileName)){
+            try{
+                String url = "http://localhost:6001/endpoint";
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type","application/json");
+                connection.setDoOutput(true);
+
+                try (OutputStream outputStream = connection.getOutputStream()) {
+                    byte[] requestBodyBytes = response.getBytes("UTF-8");
+                    outputStream.write(requestBodyBytes);
+                    outputStream.flush();
+                }
+//                DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+//                outputStream.writeBytes(response);
+//                outputStream.flush();
+//                outputStream.close();
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line;
+                    StringBuffer myResponse = new StringBuffer();
+                    while ((line = reader.readLine()) != null) {
+                        myResponse.append(line);
+                    }
+                    responseReceived = myResponse.toString();
+                    reader.close();
+                }
+            }
+            catch(Exception e){
+                System.out.println("EEE: "+e);
+            }
+        }
+        return responseReceived;
+    }
+
+    public Boolean validFile(String fileName){
+        try{
+            if(fileName==null){
+                responseReceived = "{\"file\": \"" + fileName + "\", \"error\": \"Invalid JSON input\"}";
+                System.out.println("Invalid JSON input");
+                return false;
+            }
+//            String filePath = "/C1/"+fileName;
+            String filePath = "Container1/"+fileName;
+            System.out.println(filePath);
+            File file = new File(filePath);
+            if(!file.exists()){
+                responseReceived = "{\"file\": \"" + fileName + "\", \"error\": \"File Not Found\"}";
+                System.out.println("File Not Found");
+                return false;
+            }
+        }
+        catch (Exception e){
+            System.out.println("Exception: "+e);
+        }
+        return true;
+    }
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
--- A1/Container2/src/main/java/com/A1/Container2/Controller2.java
+++ A1/Container2/src/main/java/com/A1/Container2/Controller2.java
@@ -1,10 +1,71 @@
 package com.A1.Container2;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 @RestController
 public class Controller2 {
+    String returnResponse = null;
+
+
+    @PostMapping(path = "/endpoint", consumes = "application/json")
+    public String validateData(@RequestBody String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response.toString());
+            String fileName = jsonObject.getString("file").toString();
+            System.out.println("File: "+fileName);
+            String product = jsonObject.getString("product").toString();
+            System.out.println("Product: "+product);
+            response = calculate(fileName,product);
+
+        }catch (Exception e){
+            System.out.println("Exception "+e);
+        }
+        return response;
+    }
+
+    public String calculate(String fileName, String productName){
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("Container2/"+fileName));
+            Boolean validCSV = true;
+            String line;
+            Integer sum=0, lineNumber = 0;
+            while((line = bufferedReader.readLine())!=null && validCSV == true){
+                if(line.isEmpty()){
+                    validCSV = false;
+                }
+                String[] data = line.split(",");
+
+                if(lineNumber == 0 && !data[0].equals("product") && !data[1].equals("amount")){
+                    validCSV = false;
+                }
+                if(data.length != 2){
+                    validCSV = false;
+                }
+                String product = data[0];
+                if(product.equals(productName)){
+                    Integer quantity = Integer.valueOf(data[1]);
+                    sum = sum + quantity;
+                }
+                lineNumber++;
+            }
+            if(validCSV == false){
+                returnResponse = "{\"file\": \"" + fileName + "\", \"error\": \"Input file not in CSV format.\"}";
+            }
+            returnResponse = "{\"file\": \"" + fileName + "\", \"sum\": \"" + sum + "\"}";
+        }
+        catch (Exception e){
+            System.out.println("Exception: "+e);
+        }
+        return returnResponse;
+    }
+
     @GetMapping("/greet")
     public String greet() {
         return "Greetings!";
